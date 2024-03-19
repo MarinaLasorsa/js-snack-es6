7f45cfd79df74fc0c3d10d2fab8e797ef3f3100a
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-19 17:15:34 +0100

    ADD: initial array of students + new array with the same attributes except the names are all uppercase

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-// ESERCIZIO 1
+// ESERCIZIO 1:
 
 //nome del tavolo: “Tavolo Vip”
 //lista degli invitati in ordine di posto:
@@ -27,3 +27,78 @@ const segnaposto = invitati.map((el, i) => {
 })
 
 console.log(segnaposto);
+
+
+//ESERCIZIO 2:
+
+//dobbiamo stampare le targhe col nome degli studenti: 
+//1. creare una lista contenente il loro nome tutto in maiuscolo
+    //ES (Marco della Rovere => MARCO DELLA ROVERE);
+//2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+//3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+/*Id   Name                  Grades
+213  Marco della Rovere      78
+110  Paola Cortellessa       96
+250  Andrea Mantegna 	      48
+145  Gaia Borromini          74
+196  Luigi Grimaldello 	  68
+102  Piero della Francesca   50
+120  Francesca da Polenta    84*/
+
+
+//dichiarare un array di oggetti con gli attributi specificati
+
+const studenti = [
+    {
+        id: 213,
+        name: "Marco della Rovere",
+        grades: 78
+    },
+    {
+        id: 110,
+        name: "Paola Cortellessa",
+        grades: 96
+    },
+    {
+        id: 250,
+        name: "Andrea Mantegna",
+        grades: 48
+    },
+    {
+        id: 145,
+        name: "Gaia Borromini",
+        grades: 74
+    },
+    {
+        id: 196,
+        name: "Luigi Grimaldello",
+        grades: 68
+    },
+    {
+        id: 102,
+        name: "Piero della Francesca",
+        grades: 50
+    },
+    {
+        id: 120,
+        name: "Francesca da Polenta",
+        grades: 84
+    },
+]
+
+console.log(studenti);
+
+//dichiarare nuovo array che per ogni elemento ritorna 
+//id e grades uguali ma name è maiuscolo
+
+const targheStudenti = studenti.map((el) => {
+    return {
+        id: el.id,
+        name: el.name.toUpperCase(),
+        grades: el.grades
+    }
+})
+
+console.log(targheStudenti);
+
